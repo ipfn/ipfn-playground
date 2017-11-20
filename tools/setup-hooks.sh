cc7e309bbe34482874b58985b7ab934b0eaeacc5
crackcomm <crackcomm@gmail.com>
2017-11-20 03:58:41 +0100

    fix(tools): setup-hooks.sh

--- tools/setup-hooks.sh
+++ tools/setup-hooks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-go install github.com/ipfn/ipfn/tools/ipfn-precommit
+go get -u github.com/ipfn/ipfn/tools/ipfn-precommit
 
 echo "#!/bin/sh" > .git/hooks/pre-commit
 echo "ipfn-precommit" >> .git/hooks/pre-commit
