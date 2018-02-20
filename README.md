1a5b5081621f0d690fc982593954d83e9f0fa5ce
crackcomm <crackcomm@gmail.com>
2018-02-20 22:33:29 +0100

    chore(*): first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# IPFN Synaptic Cells in Go
+
+[![IPFN project](https://img.shields.io/badge/project-IPFN-blue.svg?style=flat-square)](http://github.com/ipfn)
+[![GoDoc](https://godoc.org/github.com/ipfn/go-ipfn-synaptic?status.svg)](https://godoc.org/github.com/ipfn/go-ipfn-synaptic)
+[![Circle CI](https://img.shields.io/circleci/project/ipfn/go-ipfn-synaptic.svg)](https://circleci.com/gh/ipfn/ipfn)
+
+IPFN Synaptic cells implementation and helpers.
+
+## License
+
+                                 Apache License
+                           Version 2.0, January 2004
+                        http://www.apache.org/licenses/
+
+## Project
+
+This source code is part of [IPFN](https://github.com/ipfn) – interplanetary functions project.
---
+++ cell_test.go
@@ -0,0 +1,32 @@
+// Copyright © 2017-2018 The IPFN Developers. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package synaptic
+
+import (
+	. "testing"
+
+	"github.com/stretchr/testify/assert"
+
+	cell "github.com/ipfn/go-ipfn-cell"
+)
+
+func TestInterface(t *T) {
+	var c cell.Cell
+	c = NewCell(String, []byte("test"))
+	assert.Equal(t, c.Soul(), "/synaptic/string")
+	bytes, err := c.Memory().Bytes()
+	assert.Equal(t, err, nil)
+	assert.Equal(t, bytes, []byte("test"))
+}
