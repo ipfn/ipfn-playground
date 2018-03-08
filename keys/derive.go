f06e0f56675e95cd8cf686b6c5fe45471d6d7583
crackcomm <crackcomm@gmail.com>
2018-03-08 14:58:31 +0100

    fix(license): transfer ownership

--- keys/derive.go
+++ keys/derive.go
@@ -1,4 +1,4 @@
-// Copyright © 2017-2018 Łukasz Kurowski. All Rights Reserved.
+// Copyright © 2017-2018 The IPFN Developers. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
