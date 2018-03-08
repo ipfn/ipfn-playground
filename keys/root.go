57132583465d897f4e1b560019d9fe58d95cd3e4
crackcomm <crackcomm@gmail.com>
2018-03-08 14:55:30 +0100

    feat(*): initial commit

---
+++ keys/root.go
@@ -0,0 +1,60 @@
+// Copyright © 2017-2018 Łukasz Kurowski. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package keys
+
+import (
+	"errors"
+	"hash/crc32"
+	"strconv"
+	"strings"
+
+	prompt "github.com/segmentio/go-prompt"
+	"github.com/spf13/cobra"
+
+	"github.com/crackcomm/viperkeys"
+
+	keywallet "github.com/ipfn/go-ipfn-key-wallet"
+)
+
+// RootCmd - Root key RootCmd.
+var RootCmd = &cobra.Command{
+	Use:         "key",
+	Short:       "Keys commands",
+	Annotations: map[string]string{"category": "key"},
+}
+
+// uses global `forcePath` variable
+func deriveKey(name, path string) (_ *keywallet.ExtendedKey, err error) {
+	wallet := keywallet.NewWallet(viperkeys.Default)
+	password := prompt.PasswordMasked("Decryption password")
+	if password == "" {
+		return nil, errors.New("failed to get decryption password")
+	}
+	if forcePath {
+		path = customDerivationPath(path)
+	}
+	return wallet.Derive(name, path, []byte(password))
+}
+
+func customDerivationPath(path string) string {
+	h := crc32.NewIEEE()
+	h.Write([]byte(path))
+	s := h.Sum(nil)
+	r := []string{"m", "112"}
+	for _, v := range s {
+		r = append(r, strconv.Itoa(int(v)))
+	}
+	return strings.Join(r, "/")
+}
---
+++ seeds/import.go
@@ -0,0 +1,63 @@
+// Copyright © 2017-2018 Łukasz Kurowski. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package seeds
+
+import (
+	"errors"
+	"fmt"
+
+	prompt "github.com/segmentio/go-prompt"
+	"github.com/spf13/cobra"
+
+	cmdutil "github.com/ipfn/go-ipfn-cmd-util"
+	"github.com/ipfn/go-ipfn-key-wallet"
+
+	"github.com/crackcomm/viperkeys"
+)
+
+func init() {
+	RootCmd.AddCommand(ImportCmd)
+}
+
+// ImportCmd - Seed import command.
+var ImportCmd = &cobra.Command{
+	Use:         "import [name]",
+	Short:       "Imports existing seed",
+	Annotations: map[string]string{"category": "seed"},
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) >= 1 && viperkeys.Default.Has(args[0]) {
+			return fmt.Errorf("seed %q already exists", args[0])
+		}
+		return nil
+	},
+	Run: cmdutil.WrapCommand(HandleImportCmd),
+}
+
+// HandleImportCmd - Handles seed import command.
+func HandleImportCmd(cmd *cobra.Command, args []string) (err error) {
+	mnemonic := cmdutil.PromptConfirmed("mnemonic seed", keywallet.IsMnemonicValid)
+	password := prompt.PasswordMasked("Seed password")
+	if password == "" {
+		return errors.New("failed to get password")
+	}
+	// Ask for *unique* name
+	name := args[0]
+	if viperkeys.Default.Has(name) || name == "" {
+		name = cmdutil.PromptConfirmed("seed name", func(name string) bool {
+			return !viperkeys.Default.Has(name)
+		})
+	}
+	return viperkeys.Default.CreateKey(name, mnemonic, password)
+}
