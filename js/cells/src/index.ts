b56b5f978c7bec4992be786e2303c9bc13a4304a
crackcomm <crackcomm@gmail.com>
2018-03-10 23:19:13 +0100

    chore(clean): remove old cells

--- js/cells/src/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @license Apache-2.0
- */
-export * from './cell';
-export * from './codecs';
-export * from './builder';
-export * from './protobuf';
--- js/cells/types/multicodec/index.d.ts
+++
@@ -1,6 +0,0 @@
-
-declare module 'multicodec' {
-  export function getCodec(body: Buffer): string;
-  export function rmPrefix(body: Buffer): Buffer;
-  export function addPrefix(prefix: string, body: Buffer): Buffer;
-}
