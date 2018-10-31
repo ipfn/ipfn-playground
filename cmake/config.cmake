10ddd291cda7f2083cd6ccbd8c80aaeab8663b78
Łukasz Kurowski <crackcomm@gmail.com>
2018-10-31 18:27:06 +0100

    chore(cmake): optional build settings

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -8,6 +8,9 @@ ipfn_option(BUILD_WASM "Build WebAssembly target" OFF)
 
 ipfn_option(BUILD_DOCS "Build documentation" OFF)
 ipfn_option(BUILD_DEPS_DOCS "Build dependencies with documentation" OFF)
+ipfn_option(BUILD_DEPS_DEMOS "Build dependencies with demos" OFF)
 ipfn_option(BUILD_DEPS_TESTS "Build dependencies with tests" OFF)
+ipfn_option(BUILD_DEPS_BENCH "Build dependencies with benchmarks" OFF)
 
-ipfn_option(USE_LLVM "Build with LLVM, can be set to specific llvm-config path" OFF)
+ipfn_option(USE_TVM_RUNTIME "Build with TVM runtime" OFF)
+ipfn_option(USE_TVM_COMPILER "Build with TVM compiler" OFF)
