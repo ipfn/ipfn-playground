5815e6869d3bb3832447cec330d1f6e589dec8ad
Łukasz Kurowski <crackcomm@gmail.com>
2018-11-09 16:19:33 +0100

    fix(cmake): ccache option

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,21 +1,23 @@
 
-ipfn_option(BUILD_SMF           "Build with SMF"          OFF)
+ipfn_option(WITH_CCACHE "Build with ccache" OFF)
 
-ipfn_option(BUILD_TESTS         "Build with tests"          OFF)
+ipfn_option(BUILD_SMF "Build with SMF" OFF)
+
+ipfn_option(BUILD_TESTS "Build with tests" OFF)
 ipfn_option(BUILD_TEST_COVERAGE "Build tests with coverage" OFF)
-ipfn_option(BUILD_BENCHMARKS    "Build with benchmarks"     OFF)
+ipfn_option(BUILD_BENCHMARKS "Build with benchmarks" OFF)
 
 ipfn_option(BUILD_INTEGRATION_TESTS "Build integration tests" OFF)
 
-ipfn_option(BUILD_RUST_SDK    "Build Rust target"        OFF)
-ipfn_option(BUILD_PYTHON_SDK  "Build Python target"      OFF)
+ipfn_option(BUILD_RUST_SDK "Build Rust target" OFF)
+ipfn_option(BUILD_PYTHON_SDK "Build Python target" OFF)
 ipfn_option(BUILD_WASM_TARGET "Build WebAssembly target" OFF)
 
-ipfn_option(BUILD_DOCS       "Build documentation"                   OFF)
-ipfn_option(BUILD_DEPS_DOCS  "Build dependencies with documentation" OFF)
-ipfn_option(BUILD_DEPS_DEMOS "Build dependencies with demos"         OFF)
-ipfn_option(BUILD_DEPS_TESTS "Build dependencies with tests"         OFF)
-ipfn_option(BUILD_DEPS_BENCH "Build dependencies with benchmarks"    OFF)
+ipfn_option(BUILD_DOCS "Build documentation"  OFF)
+ipfn_option(BUILD_DEPS_DOCS "Build dependencies with documentation" OFF)
+ipfn_option(BUILD_DEPS_DEMOS "Build dependencies with demos" OFF)
+ipfn_option(BUILD_DEPS_TESTS "Build dependencies with tests" OFF)
+ipfn_option(BUILD_DEPS_BENCH "Build dependencies with benchmarks" OFF)
 
-ipfn_option(USE_TVM_RUNTIME  "Build with TVM runtime"  OFF)
+ipfn_option(USE_TVM_RUNTIME "Build with TVM runtime" OFF)
 ipfn_option(USE_TVM_COMPILER "Build with TVM compiler (includes TVM runtime)" OFF)
--- cmake/util/ccache.cmake
+++ cmake/util/ccache.cmake
@@ -1,8 +1,9 @@
 # https://github.com/ceph/ceph/blob/master/CMakeLists.txt
 # Use CCACHE_DIR environment variable
-option(WITH_CCACHE "Build with ccache.")
+
 if(WITH_CCACHE)
-  find_program (CCACHE_FOUND ccache)
+  find_program(CCACHE_FOUND ccache)
+
   if(CCACHE_FOUND)
     message(STATUS "Building with ccache: ${CCACHE_FOUND}, CCACHE_DIR=$ENV{CCACHE_DIR}")
     set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
