8c8596ad42118955f59c51864d81aed26b3b01c3
Łukasz Kurowski <crackcomm@gmail.com>
2018-11-27 23:39:19 +0100

    feat(x25519): multiple libraries benchmarks

--- tests/bench/x25519/x25519.cc
+++ tests/bench/x25519/x25519.cc
@@ -20,41 +20,54 @@
 
 #include <libhydrogen/hydro_x25519.h>
 
+#ifdef USE_SODIUM
+#include <sodium/crypto_scalarmult_curve25519.h>
+#include <sodium/crypto_scalarmult_ed25519.h>
+#endif
+
 #include <limits>
+
+#define X25519_BENCH(B_EXPR)                                                   \
+  for (auto _ : state) {                                                       \
+    state.PauseTiming();                                                       \
+    ed25519_secret_key sk1, sk2;                                               \
+    ed25519_randombytes_unsafe(sk1, sizeof(ed25519_secret_key));               \
+    ed25519_randombytes_unsafe(sk2, sizeof(ed25519_secret_key));               \
+    ed25519_public_key pk2;                                                    \
+    ed25519_publickey(sk2, pk2);                                               \
+    state.ResumeTiming();                                                      \
+    ed25519_secret_key shared;                                                 \
+    benchmark::DoNotOptimize(B_EXPR);                                          \
+    benchmark::DoNotOptimize(shared);                                          \
+  }
 
 static void
 BM_x25519(benchmark::State &state) {
-  for (auto _ : state) {
-    state.PauseTiming();
-    ed25519_secret_key sk1, sk2;
-    ed25519_randombytes_unsafe(sk1, sizeof(ed25519_secret_key));
-    ed25519_randombytes_unsafe(sk2, sizeof(ed25519_secret_key));
-    ed25519_public_key pk2;
-    ed25519_publickey(sk2, pk2);
-    state.ResumeTiming();
-    ed25519_secret_key shared;
-    x25519(shared, sk1, pk2);
-    benchmark::DoNotOptimize(shared);
-  }
+  X25519_BENCH(x25519(shared, sk1, pk2));
 }
 
 static void
 BM_hydro_x25519(benchmark::State &state) {
-  for (auto _ : state) {
-    state.PauseTiming();
-    ed25519_secret_key sk1, sk2;
-    ed25519_randombytes_unsafe(sk1, sizeof(ed25519_secret_key));
-    ed25519_randombytes_unsafe(sk2, sizeof(ed25519_secret_key));
-    ed25519_public_key pk2;
-    ed25519_publickey(sk2, pk2);
-    state.ResumeTiming();
-    uint8_t shared[hydro_x25519_BYTES];
-    hydro_x25519_scalarmult(shared, sk1, pk2, true);
-    benchmark::DoNotOptimize(shared);
-  }
+  X25519_BENCH(hydro_x25519_scalarmult(shared, sk1, pk2, true));
 }
+
+#ifdef USE_SODIUM
+static void
+BM_sodium_x25519_ed(benchmark::State &state) {
+  X25519_BENCH(crypto_scalarmult_ed25519(shared, sk1, pk2));
+}
+
+static void
+BM_sodium_x25519_ec(benchmark::State &state) {
+  X25519_BENCH(crypto_scalarmult_curve25519(shared, sk1, pk2));
+}
+#endif
 
 BENCHMARK(BM_x25519)->Arg(32)->Iterations(10000);
 BENCHMARK(BM_hydro_x25519)->Arg(32)->Iterations(10000);
+#ifdef USE_SODIUM
+BENCHMARK(BM_sodium_x25519_ed)->Arg(32)->Iterations(10000);
+BENCHMARK(BM_sodium_x25519_ec)->Arg(32)->Iterations(10000);
+#endif
 
 BENCHMARK_MAIN();
