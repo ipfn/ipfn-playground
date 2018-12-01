9d1cf71700e66cee123928d840a1bcc0b4084a98
Łukasz Kurowski <crackcomm@gmail.com>
2018-12-01 01:17:07 +0100

    feat(rust): rebuild on change

--- rust/build.rs
+++ rust/build.rs
@@ -8,6 +8,8 @@ fn main() {
     let crate_path = Path::new(&crate_dir);
     let config = cbindgen::Config::from_root_or_default(&crate_path);
 
+  println!("cargo:rerun-if-changed=src/");
+
     cbindgen::generate_with_config(&crate_path, config)
       .expect("Unable to generate bindings")
       .write_to_file("../include/ipfn/rust/bindings.h");
